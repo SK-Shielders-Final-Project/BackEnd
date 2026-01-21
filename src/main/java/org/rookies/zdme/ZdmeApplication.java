b60fa2b813e25f45e23ef77ed2a7876376f82f2b
knm6777 <knm6777@gmail.com>
2026-01-21 13:58:28 +0900

    feat: 초기 환경 세팅

---
+++ src/main/java/org/rookies/zdme/ZdmeApplication.java
@@ -0,0 +1,13 @@
+package org.rookies.zdme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZdmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZdmeApplication.class, args);
+    }
+
+}
